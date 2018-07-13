7197386271ff74d1102d285bd98749d8615ae7b6
Chinmaya.Bhondwe <chinmayab108@gmail.com>
2018-07-13 15:45:12 +0530

    Templates added

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'crudapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR + '/crudapp/templates/' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
